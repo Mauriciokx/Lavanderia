ee7bb19ceb5980ef6dbe15b60eab41e13eb3fe13
Mauricio Pacheco <Mauricio Pacheco@DESKTOP-CU0M8U2>
2024-07-23 12:08:03 -0600

    EncriptarContraseña

--- src/Modelo/Login.java
+++ src/Modelo/Login.java
@@ -12,6 +12,8 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import javax.swing.JOptionPane;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 
 /**
  *
@@ -27,11 +29,12 @@ public class Login {
     public static Connection conexion = Conexion.obtenerConexion();
 
     public static boolean buscarUsuario(String usuario, String contraseña){
-        
+        String conEncriptada = encriptar(contraseña);
         String query = "SELECT usuario FROM usuarios WHERE usuario = ? AND contraseña = ?";
         try (PreparedStatement preparedStatement = conexion.prepareStatement(query)) {
             preparedStatement.setString(1, usuario);
-            preparedStatement.setString(2, contraseña);
+            //preparedStatement.setString(2, contraseña);
+            preparedStatement.setString(2, conEncriptada);
             
             try (ResultSet resultSet = preparedStatement.executeQuery()) {
                 if (resultSet.next()) {
@@ -45,6 +48,25 @@ public class Login {
         }  
         return ban;
     }
-            
+    
+    public static String encriptar(String text) {
+        try {
+            // Crear una instancia de MessageDigest con SHA-256
+            MessageDigest md = MessageDigest.getInstance("SHA-256");
+
+            // Convertir el texto a bytes y realizar el hash
+            byte[] hashBytes = md.digest(text.getBytes());
+
+            // Convertir el hash a un string hexadecimal
+            StringBuilder sb = new StringBuilder();
+            for (byte b : hashBytes) {
+                sb.append(String.format("%02x", b));
+            }
+
+            return sb.toString();
+        } catch (NoSuchAlgorithmException e) {
+            throw new RuntimeException(e);
+        }
+    }
 
 }
